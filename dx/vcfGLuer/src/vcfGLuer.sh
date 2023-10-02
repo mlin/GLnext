58568033c0eae7697ff12ee9ec5605486ff48eee
Mike Lin <dna@mlin.net>
2023-10-01 23:25:18 -1000

    fix dx invocation

--- dx/vcfGLuer/src/vcfGLuer.sh
+++ dx/vcfGLuer/src/vcfGLuer.sh
@@ -82,7 +82,7 @@ main() {
         --conf spark.sql.adaptive.coalescePartitions.parallelismFirst=false \
         $HDFS_RETRY_CONF \
         --name vcfGLuer vcfGLuer-*.jar \
-        --manifest --tmp-dir hdfs:///tmp --config "$config" \
+        --manifest --config "$config" \
         $filter_bed_arg $filter_contigs_arg $split_bed_arg \
         vcfGLuer_in.hdfs.manifest "hdfs:///vcfGLuer/out/$output_name" \
         || true
